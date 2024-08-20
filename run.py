b02a2936dd8a2b60e350d60dc49a1df9e3968cf5
sidneygolstein <golstein.sidney@gmail.com>
2024-08-20 17:12:56 +0200

    Test get_url

--- run.py
+++ run.py
@@ -1,10 +1,16 @@
 from app import create_app
 from dotenv import load_dotenv
 from config import Config
+from .helpers import get_url  # Import your helper function
 
 
 load_dotenv()
 app = create_app()
+
+
+@app.context_processor
+def utility_processor():
+    return dict(get_url=get_url)
 
 if __name__ == '__main__':
     app.run(debug=True, port=Config.PORT)
