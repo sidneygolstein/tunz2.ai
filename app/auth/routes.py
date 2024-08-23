eba59e345775e7e16ec37a7f710112c9a9da4332
sidneygolstein <golstein.sidney@gmail.com>
2024-08-23 10:54:04 +0200

    Improvement of notification messaging for login/register/reset_pwd/request_reset_pwd

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -51,17 +51,23 @@ def send_reset_email(user, user_type):
     reset_url = get_url('auth.reset_password', token=token)
     
     if user_type == 'admin':
-        msg_body = f'''Hello admin, you requested to reset your password. Here is the link to reset the password:
-{reset_url}
+        msg_body = f'''👋 Hello admin, 
+        
+        You requested to reset your password. Here is the link to reset the password:
+        {reset_url}
 
-If you did not make this request then simply ignore this email and no changes will be made.
-'''
+        If you did not make this request, simply ignore this email.
+        '''
     else:
-        msg_body = f'''You requested to reset your password. Here is the link to reset the password:
-{reset_url}
+        msg_body = f'''👋 Hello {user.name},
+        
+        You requested to reset your password. Here is the link to reset the password:
+        {reset_url}
 
-If you did not make this request then simply ignore this email and no changes will be made.
-'''
+        If you did not make this request, simply ignore this email.
+
+        The Tunz.ai team
+        '''
 
     msg = Message('Password Reset Request',
                   sender='noreply@tunz.ai',
@@ -88,22 +94,22 @@ def is_strong_password(password):
 # ROUTES
 
 
-# To be enhanced with jwt
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
+        data = request.get_json()
+        email = data.get('email')
+        password = data.get('password')
         hr = HR.query.filter_by(email=email).first()
 
         if hr and hr.check_password(password):
             if hr.confirmed:
-                flash("Login succeeded", "success")
-                return redirect(url_for('main.home', hr_id=hr.id))
+                return jsonify({"msg": "✅ Login succeeded", "redirect_url": url_for('main.home', hr_id=hr.id)}), 200
             else:
-                return render_template('auth/login.html', error="Account not confirmed by admin")
-        return render_template('auth/login.html', error="Invalid credentials")
+                return jsonify({"msg": "👋 Hi, the TUNZ admin has not yet approved your registration. Please wait. You will receive an email as soon as you're approved."}), 403
+        return jsonify({"msg": "⛔ The email or password you entered is incorrect. Please try again. You can also reset your password if it persists."}), 400
     return render_template('auth/login.html')
+
 
 
 
@@ -125,14 +131,14 @@ def register():
         company_name = data.get('company_name')
 
         if password != confirm_password:
-            return jsonify({"msg": "Passwords do not match"}), 400
+            return jsonify({"msg": "⛔ Your 'password' and 'confirm password' do not match, please try again"}), 400
         
         if not is_strong_password(password):
-            return jsonify({"msg": "Password does not meet the criteria: At least 8 characters, one upper case letter, one digit, one special character"}), 400
+            return jsonify({"msg": "⛔ Password does not meet the criteria: At least 8 characters, one upper case letter, one digit, one special character"}), 400
 
         existing_user = HR.query.filter_by(email=email).first()
         if existing_user:
-            return jsonify({"msg": "User already exists"}), 400
+            return jsonify({"msg": "🤗 It seems that you are already registered. Please login"}), 400
         
         existing_company = Company.query.filter_by(name=company_name).first()
         if not existing_company:
@@ -166,7 +172,7 @@ def register():
         msg.body = f'New account registration request:\n\nEmail: {email}\nName: {name}\nSurname: {surname}\nCompany: {company_name}\n\nPlease review the account by visiting the following link: {dashboard_url}'
         mail.send(msg)
 
-        return jsonify({"msg": "Registration request sent to admin for confirmation. You will receive an email when your account will be confirmed"}), 200
+        return jsonify({"msg": "✅ Your registration request has been sent to the admin for confirmation. You will receive an email as soon as your account is confirmed"}), 200
     return render_template('auth/register.html')
 
 
@@ -178,7 +184,8 @@ def register():
 @auth.route('/request_reset_password', methods=['GET', 'POST'])
 def request_reset_password():
     if request.method == 'POST':
-        email = request.form['email']
+        data = request.get_json()
+        email = data.get('email')
         user = None
         user_type = None
 
@@ -194,12 +201,12 @@ def request_reset_password():
         
         if user:
             send_reset_email(user, user_type)
-            flash("Password reset request confirmed. A link was sent to your email. Please check your emails.", "success")
-            return redirect(url_for('auth.login'))
+            return jsonify({"msg": "✅ Your password reset request is confirmed. A link was sent to your email. Please check your emails."}), 200
         else:
-            flash("Email not found. Please try again.", "error")
-            return redirect(url_for('auth.request_reset_password')) 
+            return jsonify({"msg": "⛔ We did not find your email. Please try again."}), 404
+    
     return render_template('auth/request_reset_password.html')
+
 
 
 # Reset password
@@ -207,27 +214,28 @@ def request_reset_password():
 def reset_password(token):
     user, user_type = verify_reset_token(token)
     if not user:
-        flash("Invalid or expired token", "error")
-        return redirect(url_for('auth.request_reset_password'))
+        return jsonify({"msg": "⛔ Invalid or expired token. Please make a new password request."}), 400
     
     if request.method == 'POST':
-        password = request.form['password']
-        password_confirm = request.form['password_confirm']
+        data = request.get_json()
+        password = data.get('password')
+        password_confirm = data.get('password_confirm')
+
         if password == password_confirm:
             if not is_strong_password(password):
-                return jsonify({"msg": "Password does not meet the criteria: At least 8 characters, one upper case letter, one digit, one special character"}), 400
+                return jsonify({"msg": "⛔ Your password does not meet the criteria: At least 8 characters, one upper case letter, one digit, one special character"}), 400
             user.set_password(password)
             db.session.commit()
+
             if user_type == 'admin':
-                flash("Admin password reset confirmed. You can now log in.", "success")
-                return redirect(url_for('auth.admin_login'))
+                return jsonify({"msg": "✅ Admin password reset confirmed. You can now log in.", "redirect_url": url_for('auth.admin_login')}), 200
             else:
-                flash("HR password reset confirmed. You can now log in.", "success")
-                return redirect(url_for('auth.login'))
+                return jsonify({"msg": "✅ Your password has just been reset. You can now log in.", "redirect_url": url_for('auth.login')}), 200
         else:
-            flash("Passwords do not match. Please try again.", "error")
-            return redirect(url_for('auth.reset_password', token=token))
+            return jsonify({"msg": "⛔ Your 'password' and 'confirm password' do not match. Please try again."}), 400
+    
     return render_template('auth/reset_password.html', token=token)
+
 
 
 ## ADMIN AUTH ##
@@ -245,24 +253,24 @@ def admin_register():
         print('Received data:', data)
 
         if password != confirm_password:
-            return jsonify({"msg": "Passwords do not match"}), 400
+            return jsonify({"msg": "⛔ Passwords do not match"}), 400
         
         if not is_strong_password(password):
-            return jsonify({"msg": "Password does not meet the criteria: At least 8 characters, one upper case letter, one digit, one special character"}), 400
+            return jsonify({"msg": "⛔ Password does not meet the criteria: At least 8 characters, one upper case letter, one digit, one special character"}), 400
 
         if email != 'sidney@tunz.ai':
-            return jsonify({"msg": "Only the designated admin can register"}), 400
+            return jsonify({"msg": "⛔ Only the designated admin can register"}), 400
 
         existing_admin = Admin.query.filter_by(email=email).first()
         if existing_admin:
-            return jsonify({"msg": "Admin already exists"}), 400
+            return jsonify({"msg": "⛔ Admin already exists"}), 400
 
         admin = Admin(email=email, name=name, surname=surname)
         admin.set_password(password)
         db.session.add(admin)
         db.session.commit()
 
-        return jsonify({"msg": "Admin registration successful"}), 200
+        return jsonify({"msg": "✅ Admin registration successful"}), 200
     return render_template('auth/admin_register.html')
 
 
